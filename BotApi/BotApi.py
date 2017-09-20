b569d93360ed0055c6229f5101820599e9254a0b
Matthew Cooper <mcooper@servpronet.com>
2017-09-19 19:07:40 -0700

    Started working on the database creation scripts

---
+++ BotApi/BotApi.py
@@ -0,0 +1,3 @@
+def enterPlayer(twitchName, lichessName):
+   player = Player(0, twitchName, lichessName, 1500) 
+   Database.saveFunction(player)
---
+++ MySQLScripts/CreateDatabase.sql
@@ -0,0 +1,12 @@
+Create DATABASE IF NOT EXISTS PairingsBot;
+
+GRANT USAGE ON *.* TO PairingsBotMod@localhost IDENTIFIED BY 'password'; --Change this later, especially for production
+GRANT ALL PRIVILEGES ON PairingsBotDatabase.* TO PairingsBotMod@localhost;
+FLUSH PRIVILEGES;
+
+CREATE TABLE IF NOT EXISTS PairingsBot.Player (PlayerId INT NOT NULL AUTO_INCREMENT,
+                                               TwitchName Varchar(25),
+                                               LichessName Varchar(25),
+                                               Rating INT,
+                                               PRIMARY KEY (PlayerId)
+                                              );
