49c5dbd5b89f9f90de939d79d2d70a4d5743080f
Matthew Cooper <mcooper@servpronet.com>
2018-01-27 11:12:46 -0800

    Curses hello world is working

--- Dockerfile
+++ Dockerfile
@@ -5,5 +5,3 @@ ADD . /code
 WORKDIR /code
 
 RUN pip install -r ./Python/requirements.txt
-
-CMD ["python", "./Python/BotApi/BotApi.py"]
--- Python/BotApi/BotApi.py
+++ Python/BotApi/BotApi.py
@@ -2,60 +2,14 @@
 
 # Will use curses for the GUI
 
-import pymysql
+import curses
 
-def getConnection():
+scr = curses.initscr()
+scr.keypad(0)
+curses.noecho()
 
-     connection = pymysql.connect(host='pairingsbot_db_1',
-                                 user='root',
-                                 password='root',
-                                 db='pairingsbotdb',
-                                 charset='utf8mb4',
-                                 cursorclass=pymysql.cursors.DictCursor)
-     return connection
+scr.addstr("Hello world")
+scr.refresh()
+scr.getch()
 
-def testDatabase():
-    try:
-        connection = getConnection()
-        with connection.cursor() as cursor:
-            sql = "INSERT INTO Player (TwitchName, LichessName, Rating) Values (%s, %s, %s)"
-            cursor.execute(sql, ('test', 'test', 1500))
-
-            connection.commit()
-    finally:
-        connection.close()
-
-def readDatabase():
-    print("Reading the database")
-    try:
-        connection = getConnection()
-        with connection.cursor() as cursor:
-            sql = "SELECT * FROM Player"
-            cursor.execute(sql)
-            for twitchName in cursor:
-                print(twitchName)
-    finally:
-        connection.close()
-
-def initialize():
-    print("Hello!")
-    print("\nWhat would you like to do?")
-    print("\n1. Add player.")
-    print("\n2. Read database.")
-    print("\n3. Exit.")
-    number = input()
-    while number != "3":
-        if number == "1":
-            testDatabase()
-        elif number == "2":
-            readDatabase()
-        else:
-            print("Try again.")
-
-        print("\nWhat would you like to do?")
-        print("\n1. Insert into database.")
-        print("\n2. Read database.")
-        print("\n3. Exit.")
-        number = input()
-
-initialize()
+curses.endwin()
